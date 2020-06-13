b5b3977291c6c3790fefd7c2e019c3dba0b15dea
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-13 21:26:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  binding.pry
+  pigeon_list = {}
+    names = []
+    keys = []
+    i = 0
+    data.each do |key1, value1|
+        keys.push(key1)
+        data[key1].each do |key2, value2|
+
+            value2.each do |index|
+                names.push(index).uniq!
+            end
+
+
+        names.each do |item|
+            pigeon_list[item] = {keys[i] => [],
+            keys[i+1] => [],
+            keys[i+2] => []}
+        end
+
+end
+    end
 end
