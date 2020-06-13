e74e8154d728a3f283411e03cf22589004ae3a64
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-13 21:35:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
-    final_array[key] = value
+    #final_array[key] = value
     binding.pry
+    value.each do |inner_key, names|
+      binding.pry
   end
 
 end
