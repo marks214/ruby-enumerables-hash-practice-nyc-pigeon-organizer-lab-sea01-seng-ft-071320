64b775e7c6b88ce59f8d2410af7ac7dfd6fdd1c2
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-13 21:55:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,14 +4,13 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        binding.pry
         if !final_aray[name]
           final_aray[name] = {}
         end
         if !final_array[name][key]
-          final_array[name][key] = {}
+          final_array[name][key] = []
         end
-        binding.pry
+        final_array[name][key].push(inner_key)
       end
       binding.pry
     end
